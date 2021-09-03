36d04ee795e4ba33eae0e0be99621fc21fcccb76
Gobe Hobona <ghobona@ogc.org>
2021-09-03 12:33:45 +0100

    updated license text in the scripts

--- src/main/resources/SFSQL_1-1_Compliance_Test_Suite_Test_Resources/NormalizedGeometry/sqlnsch.sql
+++ src/main/resources/SFSQL_1-1_Compliance_Test_Suite_Test_Resources/NormalizedGeometry/sqlnsch.sql
@@ -6,10 +6,9 @@
 --
 -- Copyright 1998, Open GIS Consortium, Inc.
 --
--- The material in this document details an Open GIS Consortium Test Suite in
--- accordance with a license that your organization has signed. Please refer
--- to http://www.opengeospatial.org/testing/ to obtain a copy of the general license
--- (it is part of the Conformance Testing Agreement). 
+-- The material is licensed under the Apache License, Version 2.0 (the "License"). You may obtain a copy of the License from http://opensource.org/licenses/Apache-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. 
 --
 --//////////////////////////////////////////////////////////////////////////////
 --
